d5f7a9ead73ed77990bd85175f191d85708be84a
Art Gonzalez <arturo.gonzalez@austincc.edu>
2020-02-06 22:21:54 -0600

    classwork 2 solution

--- src/classwork/02_assign/decision.cpp
+++ src/classwork/02_assign/decision.cpp
@@ -1,1 +1,18 @@
 //Write the function code that returns the product of hours and hourly_rate.
+#include "decision.h"
+double gross_pay(double hours, double hourly_rate)
+{
+	double pay;
+	if (hours <= 40)
+	{
+		pay = hours * hourly_rate;
+	}
+	else
+	{
+		pay = 40 * hourly_rate;
+		double ot_hours = hours - 40;
+		pay = pay + (ot_hours * 1.5 * hourly_rate);
+	}
+
+	return pay;
+}
