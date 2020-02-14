21f7fbf20ddf7fac3963e56ff62472519d33903a
moneeb18 <moneeb.abu-esba@g.austincc.edu>
2020-02-13 21:53:21 -0600

    Moneeb18 Homewrok 2

--- test/homework_test/02_decisions_test/02_decisions_tests.cpp
+++ test/homework_test/02_decisions_test/02_decisions_tests.cpp
@@ -6,16 +6,18 @@ TEST_CASE("Verify Test Configuration", "verification") {
 }
 TEST_CASE("Test get grade points")
 {
-REQUIRE(get_grade_points("A")==4)
-
-
+	REQUIRE(get_grade_points("A") == 4);
+	REQUIRE(get_grade_points("B") == 3);
+	REQUIRE(get_grade_points("C") == 2);
+	REQUIRE(get_grade_points("D") == 1);
+	REQUIRE(get_grade_points("F") == 0);
 
 }
 TEST_CASE("Test calculate GPA")
 {
-REQUIRE(calculate_gpa(12,45)==3.75)
-
-
+	REQUIRE(calculate_gpa(12, 45) == 3.75);
+	REQUIRE(calculate_gpa(120, 390) == 3.25);
+	REQUIRE(calculate_gpa(90, 180) == 2.00);
 
 
 }
