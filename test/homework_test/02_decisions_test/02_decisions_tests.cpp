2dc53d5224e2722b3ec0def55f452365770e13fc
Moneeb <moneeb.abu-esba@g.austincc.edu>
2020-02-18 17:47:09 -0600

    moneeb abu-Esba classwork

--- test/homework_test/02_decisions_test/02_decisions_tests.cpp
+++ test/homework_test/02_decisions_test/02_decisions_tests.cpp
@@ -1,4 +1,4 @@
-#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
++99#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
 
 TEST_CASE("Verify Test Configuration", "verification") {
