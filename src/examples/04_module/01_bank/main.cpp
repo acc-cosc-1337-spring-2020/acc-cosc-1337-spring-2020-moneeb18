13ff4a3c21dd0ba3523c10519754b0d69b475478
Moneeb <moneeb.abu-esba@g.austincc.edu>
2020-02-25 19:40:53 -0600

    moneeb abuesba

--- src/examples/04_module/01_bank/main.cpp
+++ src/examples/04_module/01_bank/main.cpp
@@ -1,6 +1,17 @@
-
+#include "bank_account.h"
+#include <iostream>
+using std::cout;using std::cin;
 int main()
 {
 
+	
+	BankAccount account(500);
+	auto balance = account.get_balance();
+	cout << "Balance is: " << balance;
+	auto amount{ 0 };
+	cout << "Enter deposit amount: ";
+	cin >> amount;
+	account.deposit(amount);
+	cout << "Balance is: " << balance;
 	return 0;
 }
