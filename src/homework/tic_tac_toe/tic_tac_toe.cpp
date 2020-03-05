80cec8cbc08be1bc81253125ce26ec6ae094e6a2
moneeb18 <moneeb.abu-esba@g.austincc.edu>
2020-03-05 15:37:29 -0600

    moneeb

--- src/homework/tic_tac_toe/tic_tac_toe.cpp
+++ src/homework/tic_tac_toe/tic_tac_toe.cpp
@@ -1,1 +1,39 @@
 //cpp
+#include <iostream>
+Board::Board()
+{
+	turn = 'X';
+	for (int i = 0; i < 9; i++)
+	{
+		boardArr[i] =' ';
+	}
+}
+
+void Board::print()
+{
+	std::cout << "[" << boardArr[0] << "] ";
+	std::cout << "[" << boardArr[1] << "] ";
+	std::cout << "[" << boardArr[2] << "]\n ";
+	std::cout << "[" << boardArr[3] << "] ";
+	std::cout << "[" << boardArr[4] << "] ";
+	std::cout << "[" << boardArr[5] << "]\n ";
+	std::cout << "[" << boardArr[6] << "] ";
+	std::cout << "[" << boardArr[7] << "] ";
+	std::cout << "[" << boardArr[8] << "]\n ";
+
+}
+
+void Board::changeTurn()
+{
+	turn = (turn == 'X') ? 'O' : 'X';
+}
+bool Board::makeMove(int pos)
+{
+	if (pos < 1 || poz >9) {
+		return false;
+	}
+	if (boardArr[pos] != ' ') {
+		return false;
+	}
+	return;
+}
--- src/homework/tic_tac_toe/tic_tac_toe.h
+++ src/homework/tic_tac_toe/tic_tac_toe.h
@@ -1,1 +1,14 @@
 //h
+#ifndef BOARD_H
+#define BOARD_H
+class Board
+{
+	char turn;
+	char boardArr[9];
+
+public:
+	Board();
+	void print();
+	void changeTurn();
+	bool makeMove(int pos);
+};
