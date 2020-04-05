d3664488c3947036d051a214a679b29758b15f64
moneeb18 <moneeb.abu-esba@g.austincc.edu>
2020-04-04 22:12:32 -0500

    moneeb

--- src/homework/tic_tac_toe/tic_tac_toe.cpp
+++ src/homework/tic_tac_toe/tic_tac_toe.cpp
@@ -108,48 +108,56 @@ void TicTacToe::clear_board()
 
 bool TicTacToe::check_column_win()
 {
-	if (pegs[0]==player && pegs[3]== player && pegs[6] == player)
+	if (pegs[0]=="X" && pegs[3]== "X" && pegs[6] == "X")
 	{
 		return true;
 	}
-	else if (pegs[1] == player && pegs[4] == player && pegs[7] == player)
+	else if (pegs[0] == "O" && pegs[3] == "O" && pegs[6] == "O") { return true; }
+	else if (pegs[1] == "X" && pegs[4] == "X" && pegs[7] == "X")
 	{
 		return true;
 	}
-	else if (pegs[2] == player && pegs[5] == player && pegs[8] == player)
+	else if (pegs[1] == "O"&& pegs[4] ==  "O"&& pegs[7] == "O") { return true; }
+	else if (pegs[2] == "X" && pegs[5] == "X" && pegs[8] == "X")
 	{
 		return true;
 	}
+	else if (pegs[2] == "O" && pegs[5] == "O" && pegs[8] == "O") { return true; }
 	return false;
 }
 
 bool TicTacToe::check_row_win()
 {
-	if (pegs[0] == player && pegs[1] == player && pegs[2] == player)
+	if (pegs[0] == "X" && pegs[1] =="X" && pegs[2] == "X")
 	{
 		return true;
 	}
-	else if (pegs[3] == player && pegs[4] == player && pegs[5] == player)
+	else if (pegs[0] == "O" && pegs[1] == "O" && pegs[2] == "O") { return true; }
+	else if (pegs[3] == "X" && pegs[4] =="X" && pegs[5] == "X")
 	{
 		return true;
 	}
-	else if (pegs[6] == player && pegs[7] == player && pegs[8] == player)
+	else if (pegs[3] == "O" && pegs[4] == "O" && pegs[5] == "O") { return true; }
+	else if (pegs[6] == "X" && pegs[7] == "X" && pegs[8] == "X")
 	{
 		return true;
 	}
+	else if (pegs[6] == "O" && pegs[7] == "O" && pegs[8] == "O") { return true; }
 	return false;
 }
 
 bool TicTacToe::check_diagonal_win()
 {
-	if (pegs[0] == player && pegs[4] == player && pegs[8] == player)
+	if (pegs[0] == "X"&& pegs[4] == "X" && pegs[8] == "X")
 	{
 		return true;
 	}
-	else if (pegs[6] == player && pegs[4] == player && pegs[2] == player)
+	else if (pegs[0] == "O"&& pegs[4] == "0" && pegs[8] == "O") { return true; }
+	else if (pegs[6] =="X" && pegs[4] == "X" && pegs[2] == "X")
 	{
 		return true;
 	}
+	else if (pegs[6] == "O" && pegs[4] == "O" && pegs[2] == "O") { return true; }
 	return false;
 }
 
