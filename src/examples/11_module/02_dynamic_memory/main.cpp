c655ed8f1cbe49dbca4343f1cc70522c7e5d6e99
moneeb18 <moneeb.abu-esba@g.austincc.edu>
2020-04-16 18:56:02 -0500

    moneeb

--- src/examples/11_module/02_dynamic_memory/main.cpp
+++ src/examples/11_module/02_dynamic_memory/main.cpp
@@ -1,5 +1,18 @@
+#include<iostream>
+using std::cout;
+
+
+
+
 int main() 
 {
-	
+	int* ptr_num = new int(5);
+	cout << "Address of ptr_num: " << &ptr_num << "\n";
+	cout<<"Address ptr_num points to: " << ptr_num << "\n";
+	cout << "Value of address ptr_num points to: " << *ptr_num<<"\n";
+
+	delete ptr_num;
+	ptr_num = nullptr;
+
 	return 0;
 }
