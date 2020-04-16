f5f82d5c720880720765e0c4364e6d042f58e0a7
moneeb18 <moneeb.abu-esba@g.austincc.edu>
2020-04-16 18:41:12 -0500

    moneeb

--- src/examples/11_module/01_ref_pointers/main.cpp
+++ src/examples/11_module/01_ref_pointers/main.cpp
@@ -1,8 +1,32 @@
 #include "ref_pointers.h"
 #include<iostream>
 
+
+using std::cout;
 int main() 
 {
+	int r = 0, p = 0;
+	int& ref_r = r;
 	
+	cout << "Address of r: " << &r<<"\n";
+	cout << "Value of r" << r<<"\n";
+	cout << "Adress of ref_r" << &ref_r<<"\n";
+
+
+	int* ptr_p = &p;
+	cout <<"\n"<< "Address of P: " << &p;
+	cout <<"\n"<< "Value of p " <<p<< "\n";
+	cout << "\n" << "Address of ptr_p: " << &ptr_p<<"\n";
+
+
+
+	cout << "Address of ptr_p " << ptr_p << "\n";
+	cout << "Value of ptr_p points to: " << *ptr_p;
+
+
+	*ptr_p = 15;
+	cout << "\n"<<*ptr_p;
+	cout << "\n" << p;
+
 	return 0;
 }
--- src/examples/11_module/01_ref_pointers/ref_pointers.cpp
+++ src/examples/11_module/01_ref_pointers/ref_pointers.cpp
@@ -1,4 +1,8 @@
 #include "ref_pointers.h"
 
 //
-
+void ref_pointers(int & int1, int * int2)
+{
+	int1=100;
+	*int2 = 66;
+}
