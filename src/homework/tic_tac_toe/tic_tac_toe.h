863f02c20ec6157ff5dd52eef5f40ca01e81592d
moneeb18 <moneeb.abu-esba@g.austincc.edu>
2020-04-17 10:36:24 -0500

    moneeb

--- src/homework/tic_tac_toe/tic_tac_toe.h
+++ src/homework/tic_tac_toe/tic_tac_toe.h
@@ -23,23 +23,29 @@ public:
 	std::string get_player() const { return player; }
 	void mark_board(int position);
 	void display_board()const;
-	std::string get_winner()const {return winner;};
+	std::string get_winner()const { return winner; };
 	//friend std::istream& operator>>(std::istream & in, const TicTacToe & mark_board);
 	//friend std::ostream& operator<<(std::ostream & out, const TicTacToe & display_board);
 	friend std::istream& operator>>(std::istream & in, TicTacToe& game);
 	friend std::ostream& operator<<(std::ostream & out, const TicTacToe& game);
-
+	TicTacToe(int s):pegs(s*s," "){};
 private:
 	void set_next_player();
 	bool check_board_full();
 	void clear_board();
 	std::string player;
+	//std::vector<std::string>pegs{ 9," " };
+	//bool check_column_win();
+	//bool check_row_win();
+	//bool check_diagonal_win();
+	void set_winner();
+	std::string winner;
+
+protected:
 	std::vector<std::string>pegs{ 9," " };
 	bool check_column_win();
 	bool check_row_win();
 	bool check_diagonal_win();
-	void set_winner();
-	std::string winner;
 };
 
 class TicTacToeManager
