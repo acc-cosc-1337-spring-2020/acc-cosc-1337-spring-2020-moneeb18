614c5cb49608ceae6747e05c5b616cb8a16dd203
moneeb18 <moneeb.abu-esba@g.austincc.edu>
2020-04-23 15:19:12 -0500

    moneeb

--- src/homework/tic_tac_toe/tic_tac_toe.h
+++ src/homework/tic_tac_toe/tic_tac_toe.h
@@ -16,10 +16,7 @@ public:
 	void start_game(std::string first_player);
 	std::string get_player() const { return player; }
 	void mark_board(int position);
-	//void display_board()const;
 	std::string get_winner()const { return winner; };
-	//friend std::istream& operator>>(std::istream & in, const TicTacToe & mark_board);
-	//friend std::ostream& operator<<(std::ostream & out, const TicTacToe & display_board);
 	friend std::istream& operator>>(std::istream & in, TicTacToe& game);
 	friend std::ostream& operator<<(std::ostream & out, const TicTacToe& game);
 	TicTacToe(int s):pegs(s*s," "){};
