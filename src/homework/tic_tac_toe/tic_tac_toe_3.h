75719166c4a14ea70525ed622196e756370e3bb3
moneeb18 <moneeb.abu-esba@g.austincc.edu>
2020-04-30 21:19:00 -0500

    moneeb

--- src/homework/tic_tac_toe/tic_tac_toe_3.h
+++ src/homework/tic_tac_toe/tic_tac_toe_3.h
@@ -5,7 +5,7 @@ class TicTacToe3 : public TicTacToe
 {
 public:
 	TicTacToe3() :TicTacToe(3) {};
-	TicTacToe3(std::vector<std::string>p, std::string winner);
+	TicTacToe3(std::vector<std::string>p, std::string winner) : TicTacToe(p, winner) {};
 private: 
 	bool check_column_win();
 	bool check_row_win();
--- src/homework/tic_tac_toe/tic_tac_toe_4.h
+++ src/homework/tic_tac_toe/tic_tac_toe_4.h
@@ -5,7 +5,7 @@ class TicTacToe4 : public TicTacToe
 {
 public:
 	TicTacToe4() :TicTacToe(4) {};
-	TicTacToe4(std::vector<std::string>p, std::string winner);
+	TicTacToe4(std::vector<std::string>p, std::string winner) : TicTacToe(p, winner) {};
 private:
 	bool check_column_win();
 	bool check_row_win();
